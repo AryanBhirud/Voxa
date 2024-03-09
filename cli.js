5a3b23d1967d742050836a8561431e9aeea9e4bf
Aryan <aryan.bhirud@hotmail.com>
2024-03-09 19:19:30 +0530

    Add setup for mern & apiKey for api testing

--- cli.js
+++ cli.js
@@ -5,9 +5,10 @@ import { Command } from "commander";
 import Configstore from "configstore";
 import fs from "fs";
 import inquirer from "inquirer";
-
+import shell from 'shelljs';
 const program = new Command();
 const config = new Configstore("voxa");
+
 
 program.version("1.0.0").description("A simple CLI application");
 
@@ -38,9 +39,25 @@ program
                     name: "header",
                     message: "Enter a custom header (key:value):",
                 },
+                {
+                    type: "confirm",
+                    name: "customApiKey",
+                    message: "Do you want to add an API Key?",
+                    default: false,
+                },
+                {
+                    when: (response) => response.customApiKey,
+                    type: "input",
+                    name: "apiKey",
+                    message: "Enter an API Key: ",
+                }
             ]);
 
             config.set("baseUrl", answers.baseUrl);
+            
+            if(answers.apiKey) {
+                config.set("apiKey", answers.apiKey);
+            }
 
             if (answers.header) {
                 headers.push(answers.header);
@@ -115,6 +132,47 @@ program
 
             process.exit(1);
         }
+    });
+
+
+    
+
+    program
+    .command('setup')
+    .description('Setup development environments seamlessly')
+    .action(async () => {
+      const answer = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'devEnv',
+          message: 'Select development environment',
+          choices: [
+            { name: 'MERN Project', value: 'mern' },
+            { name: 'Django', value: 'django' },
+          ],
+        },
+      ]);
+  
+      console.log('Chosen Option:', answer.devEnv);
+  
+      if (answer.devEnv === 'mern') {
+        shell.mkdir('-p', './backend/src/controllers');
+        shell.mkdir('-p', './backend/src/models');
+        shell.mkdir('-p', './backend/src/routes');
+        shell.touch('./backend/.gitignore');
+        shell.touch('./backend/src/app.js');
+        shell.touch('./backend/src/server.js');
+  
+        shell.cd('backend');
+        shell.exec('npm init -y');
+        shell.exec('npm install express mongoose cors');
+        shell.cd('..');
+  
+        shell.cd('frontend');
+        shell.exec('npm init -y');
+        shell.exec('npx create-react-app .');
+        shell.cd('..');
+      }
     });
 
 program.parse(process.argv);
